93b1133d0cb1d40389ace09b28e3319f28496fb5
Luca_Rold <lucarold@gmail.com>
2024-12-11 10:20:42 +0000

    First commit Modified readme and clean version of main

--- README.md
+++ README.md
@@ -1,19 +1,9 @@
-# slow_costs
+# Budget Management App
 
-A new Flutter project created with FlutLab - https://flutlab.io
+## Descrizione
+Un'applicazione Flutter per la gestione delle spese, con supporto per:
+- Login e autenticazione dell'utente.
+- Gestione di spese, budget e categorie.
+- Modalità offline e sincronizzazione con Supabase come backend.
 
-## Getting Started
-
-A few resources to get you started if this is your first Flutter project:
-
-- https://flutter.dev/docs/get-started/codelab
-- https://flutter.dev/docs/cookbook
-
-For help getting started with Flutter, view our
-https://flutter.dev/docs, which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
-
-## Getting Started: FlutLab - Flutter Online IDE
-
-- How to use FlutLab? Please, view our https://flutlab.io/docs
-- Join the discussion and conversation on https://flutlab.io/residents
+## Struttura del progetto
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'Gestione Spese',
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('Gestione Spese'),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+        child: Text('Benvenuto nella tua app di gestione spese!'),
       ),
     );
   }
